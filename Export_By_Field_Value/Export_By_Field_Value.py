b79001eb50b341bf25779cee3460d46d0d63355d
Chandan Saha <saha.chandan94@gmail.com>
2018-04-07 02:27:18 +0530

    Added feature of Attribute Value Validation

--- Export_By_Field_Value/Export_By_Field_Value.py
+++ Export_By_Field_Value/Export_By_Field_Value.py
@@ -12,12 +12,12 @@ import arcpy, os
 
 fieldName = ""    #Insert FIELD NAME by which attribute value you want to export Shape File.
 
-
-# Creating Map Document Object from Current
+# Creating Map Document Object from Current Map Window
 mxd = arcpy.mapping.MapDocument("Current")
 
 hasFound = False
 
+#Checking fieldName Variable empty or not
 if (fieldName == ""):
 	print "'"'fieldName'"' Variable is Blank. Please Insert FIELD NAME!."
 
@@ -34,7 +34,7 @@ else:
 		#Created a variable which store the name of output folder
 		folderName = fieldName + "_" + str(featureName)
 		
-		
+		#List all Field Name
 		fieldObjList = arcpy.ListFields(lyr) 
 		      
 		for eachField in fieldObjList:
@@ -44,14 +44,22 @@ else:
 				with arcpy.da.SearchCursor(lyr, fieldName) as cursor:
 					attributeValues = sorted({row[0] for row in cursor})
 					
-				
+				#Creating Folder
 				arcpy.CreateFolder_management(featurePath, folderName)
+				print ('"' + folderName + '" directory has been created in: ' + str(featurePath))
 
 				for c in attributeValues:
+				
+					#Selecting Rows by unique attribute value
 					arcpy.SelectLayerByAttribute_management(featureName, 'NEW_SELECTION', '"'+fieldName+'"'"=""'" +str(c)+"'")
-					print str(c)
-					arcpy.CopyFeatures_management(featureName, os.path.join (featurePath, folderName, str(c)), '#', '0', '0', '0')
-
+					
+					#Validating Attribute Value for exporting
+					validatedAtrbName = arcpy.ValidateFieldName(str(c))
+					
+					#Copying features in above generated folder
+					arcpy.CopyFeatures_management(featureName, os.path.join (featurePath, folderName, validatedAtrbName), '#', '0', '0', '0')
+					print ('Selected Attribute Value: "' + str(c) + '" | Exported Feature Class Name: "' + validatedAtrbName + '"')
+				#Clear Selection
 				arcpy.SelectLayerByAttribute_management(featureName, "CLEAR_SELECTION")
 				attributeValues[:] = []
 				fieldObjList[:] = []
@@ -59,7 +67,6 @@ else:
         
 		if (hasFound == False):
 			print ('"' + fieldName +'"' + ' Field not exist in ' + '"' + featureName + '"' + ' Feature! Insert correct FIELD NAME.')
-	
 		hasFound = False
 del mxd
 
