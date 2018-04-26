dd368b6a632d6b2f00502f9f13caa7e32fcebebb
Chandan Saha <saha.chandan94@gmail.com>
2018-04-26 22:44:32 +0530

    Missing_ID_Founder Initial Commit

---
+++ Missing_ID_Founder/Missing_ID_Founder.py
@@ -0,0 +1,30 @@
+import arcpy
+import random
+
+sourceLayer = ""
+targetLayer = ""
+fieldName = ""
+
+sourceList = [row[0] for row in arcpy.da.SearchCursor(sourceLayer, fieldName)]
+targetList = [row[0] for row in arcpy.da.SearchCursor(targetLayer, fieldName)]
+
+missingList = [c for c in targetList if c not in sourceList]
+
+if len(missingList) != 0:
+
+    commentField = "Com_{}".format(random.randint(1, 99999))
+    arcpy.AddField_management(targetLayer, commentField, "TEXT", "", "", "20")
+
+    with arcpy.da.UpdateCursor(targetLayer, ([fieldName, commentField])) as cursor:
+        for row in cursor:
+            for missing in missingList:
+                if row[0] == missing:
+                    row[1] = "MISSING"
+                cursor.updateRow(row)
+
+else:
+    print("Hurrah! All ID mathced, no Missing in Target Layer")
+
+del cursor, sourceList, targetList, missingList
+
+# End of Script
